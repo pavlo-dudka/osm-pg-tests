750acade9b7859b6fc1a8005df4450e7fea7f5e8
pavlo-dudka <pavlo.dudka@gmail.com>
2013-08-28 14:58:00 +0300

    loading modified: only changes, not fully reload

--- load.data.into.db.bat
+++ load.data.into.db.bat
@@ -1,4 +1,5 @@
 cd binaries
+move ..\temp\ua.filtered.pbf ..\temp\ua.filtered.old.pbf
 call osmconvert.exe ..\temp\ua.osm.pbf -o=..\temp\ua.o5m
 call osmfilter.exe ..\temp\ua.o5m --keep= --keep-ways="highway=motorway =motorway_link =trunk =trunk_link =primary =primary_link =secondary =secondary_link =tertiary =tertiary_link =unclassified =residential =living_street" -o=..\temp\ua.roads.0.o5m
 call osmconvert.exe ..\temp\ua.roads.0.o5m -B=..\poly\poly.ukr.pol -o=..\temp\ua.roads.o5m
@@ -14,8 +15,11 @@ call osmconvert.exe ..\temp\ua.multipolygons.0.o5m -B=..\poly\poly.ukr.pol --com
 call osmconvert.exe ..\temp\ua.roads.o5m ..\temp\ua.places.o5m ..\temp\ua.boundaries.o5m ..\temp\ua.routes.o5m ..\temp\ua.relations.o5m ..\temp\ua.multipolygons.o5m -o=..\temp\ua.filtered.pbf
 del ..\temp\*.o5m
 
-call osmosis-latest\bin\osmosis --ts user="postgres" password="postgres" host="localhost:5432"
-call osmosis-latest\bin\osmosis --rb ..\temp\ua.filtered.pbf --lp --ws user="postgres" password="postgres" host="localhost:5432" nodeLocationStoreType="InMemory"
+if     exist ..\temp\ua.filtered.old.pbf (call osmosis --rb ..\temp\ua.filtered.pbf --sort --rb ..\temp\ua.filtered.old.pbf --sort --dc --wsc user="postgres" password="postgres" host="localhost:5432")
+if not exist ..\temp\ua.filtered.old.pbf (call osmosis --ts user="postgres" password="postgres" host="localhost:5432")
+if not exist ..\temp\ua.filtered.old.pbf (call osmosis --rb ..\temp\ua.filtered.pbf --lp --ws user="postgres" password="postgres" host="localhost:5432" nodeLocationStoreType="InMemory")
+del ..\temp\ua.filtered.old.pbf
+
 "c:\Program Files\PostgreSQL\9.2\bin\psql.exe" -U postgres -w -d osm -p 5432 -f ..\sql\osm.boundaries.sql -q
 
 cd ..
