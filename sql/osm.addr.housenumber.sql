30d90da227cc560b465564460b20b50b707a4408
pavlo-dudka <pavlo.dudka@gmail.com>
2013-09-05 18:47:46 +0300

    addr.housenumber: removed shops

--- sql/osm.addr.housenumber.sql
+++ sql/osm.addr.housenumber.sql
@@ -2,7 +2,7 @@ select 'name:';
 select w.id,wt.v
 from ways w
 inner join way_tags wt on wt.way_id=w.id and wt.k in ('addr:housename','name')
-left  join way_tags wt2 on wt2.way_id=w.id and wt2.k in ('amenity','addr:street') and wt2.v in ('cafe','restaurant','bar','Регенераторна вулиця')
+left  join way_tags wt2 on wt2.way_id=w.id and (wt2.k in ('amenity','shop','addr:street') and wt2.v in ('cafe','restaurant','bar','alcohol','convenience','Регенераторна вулиця'))
 where
  wt2.k is null and (
  trim(wt.v)  similar to '[1-9][0-9\-]*[а-яєі]?(/[1-9][0-9]*[а-я]?)?( к[1-9][0-9]*)?' or         --trim it
@@ -10,6 +10,7 @@ lower(wt.v)  similar to '[1-9][0-9\-]*[а-яєі]?(/[1-9][0-9]*[а-я]?)?( к[1-
        wt.v  similar to '[1-9][0-9\-]*[a-zA-Z]?(/[1-9][0-9]*[a-zA-Z]?)?' or                     --make it cyrilic
 lower(wt.v)  similar to '[1-9][0-9\-]*[а-яєі]?(/[1-9][0-9]*[а-я]?)?( ?к\.? ?[1-9][0-9]*)?' or   --add space before "k", remove space after "k"
 lower(wt.v)  similar to '[1-9][0-9\-]*[-/ ][а-яєі]' or                                          --remove "-/ " before character
+       position('`' in wt.v)>0 or                                                               --remove "`"
        wt.v  in ('-','*','?','0','00'))
 order by 1;
 
@@ -26,5 +27,6 @@ lower(wt.v)  similar to '[1-9][0-9\-]*[а-яєі]?(/[1-9][0-9]*[а-я]?)?( к[1-
        wt.v  similar to '[1-9][0-9\-]*[a-zA-Z]?(/[1-9][0-9]*[a-zA-Z]?)?' or                     --make it cyrilic
 lower(wt.v)  similar to '[1-9][0-9\-]*[а-яєі]?(/[1-9][0-9]*[а-я]?)?( ?к\.? ?[1-9][0-9]*)?' or   --add space before "k", remove space after "k"
 lower(wt.v)  similar to '[1-9][0-9\-]*[-/ ][а-яєі]' or                                          --remove "-/ " before character
+       position('`' in wt.v)>0 or                                                               --remove "`"
        wt.v  in ('-','*','?','0','00'))
 order by 1;
