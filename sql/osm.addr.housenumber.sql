af567d013ae64900414f25334789fe7606a59988
pavlo-dudka <pavlo.dudka@gmail.com>
2013-09-06 18:14:05 +0300

    addr.housenumber: check nodes

--- sql/osm.addr.housenumber.sql
+++ sql/osm.addr.housenumber.sql
@@ -2,7 +2,7 @@ select 'name:';
 select w.id,wt.v
 from ways w
 inner join way_tags wt on wt.way_id=w.id and wt.k in ('addr:housename','name')
-left  join way_tags wt2 on wt2.way_id=w.id and (wt2.k in ('amenity','shop','addr:street') and wt2.v in ('cafe','restaurant','bar','alcohol','convenience','Регенераторна вулиця'))
+left  join way_tags wt2 on wt2.way_id=w.id and (wt2.k in ('amenity','shop','addr:street','amenity') and wt2.v in ('cafe','restaurant','bar','alcohol','convenience','Регенераторна вулиця','school','kindergarten'))
 where
  wt2.k is null and (
  trim(wt.v)  similar to '[1-9][0-9\-]*[а-яєі]?(/[1-9][0-9]*[а-я]?)?( к[1-9][0-9]*)?' or         --trim it
@@ -19,6 +19,40 @@ select 'addr:housenumber:';
 select w.id,wt.v
 from ways w
 inner join way_tags wt on wt.way_id=w.id and wt.k='addr:housenumber'
+where
+       wt.v not similar to '[1-9][0-9\-]*[а-яєі]?(/[1-9][0-9]*[а-я]?)?( к[1-9][0-9]*)?|[1-9][0-9]*-[1-9][0-9]*|бос [1-9][0-9]*' and
+(
+ trim(wt.v)  similar to '[1-9][0-9\-]*[а-яєі]?(/[1-9][0-9]*[а-я]?)?( к[1-9][0-9]*)?' or         --trim it
+lower(wt.v)  similar to '[1-9][0-9\-]*[а-яєі]?(/[1-9][0-9]*[а-я]?)?( к[1-9][0-9]*)?' or         --make it lower
+       wt.v  similar to '[1-9][0-9\-]*[a-zA-Z]?(/[1-9][0-9]*[a-zA-Z]?)?' or                     --make it cyrilic
+lower(wt.v)  similar to '[1-9][0-9\-]*[а-яєі]?(/[1-9][0-9]*[а-я]?)?( ?к\.? ?[1-9][0-9]*)?' or   --add space before "k", remove space after "k"
+lower(wt.v)  similar to '[1-9][0-9\-]*[-/ ][а-яєі]' or                                          --remove "-/ " before character
+       position('`' in wt.v)>0 or                                                               --remove "`"
+       wt.v  in ('-','*','?','0','00'))
+order by 1;
+
+select '';
+select 'name:';
+select w.id,wt.v
+from nodes w
+inner join node_tags wt on wt.node_id=w.id and wt.k in ('addr:housename','name')
+left  join node_tags wt2 on wt2.node_id=w.id and (wt2.k in ('amenity','shop','addr:street','highway') and wt2.v in ('cafe','restaurant','bar','alcohol','convenience','Регенераторна вулиця','milestone'))
+where
+ wt2.k is null and (
+ trim(wt.v)  similar to '[1-9][0-9\-]*[а-яєі]?(/[1-9][0-9]*[а-я]?)?( к[1-9][0-9]*)?' or         --trim it
+lower(wt.v)  similar to '[1-9][0-9\-]*[а-яєі]?(/[1-9][0-9]*[а-я]?)?( к[1-9][0-9]*)?' or         --make it lower
+       wt.v  similar to '[1-9][0-9\-]*[a-zA-Z]?(/[1-9][0-9]*[a-zA-Z]?)?' or                     --make it cyrilic
+lower(wt.v)  similar to '[1-9][0-9\-]*[а-яєі]?(/[1-9][0-9]*[а-я]?)?( ?к\.? ?[1-9][0-9]*)?' or   --add space before "k", remove space after "k"
+lower(wt.v)  similar to '[1-9][0-9\-]*[-/ ][а-яєі]' or                                          --remove "-/ " before character
+       position('`' in wt.v)>0 or                                                               --remove "`"
+       wt.v  in ('-','*','?','0','00'))
+order by 1;
+
+select '';
+select 'addr:housenumber:';
+select w.id,wt.v
+from nodes w
+inner join node_tags wt on wt.node_id=w.id and wt.k='addr:housenumber'
 where
        wt.v not similar to '[1-9][0-9\-]*[а-яєі]?(/[1-9][0-9]*[а-я]?)?( к[1-9][0-9]*)?|[1-9][0-9]*-[1-9][0-9]*|бос [1-9][0-9]*' and
 (
