16e0b22830e294986df4e8117cc72d350da3ddb1
pavlo-dudka <pavlo.dudka@gmail.com>
2013-09-09 18:03:44 +0300

    street.relations.2: housenumber export

--- sql/osm.street.relations.2.sql
+++ sql/osm.street.relations.2.sql
@@ -4,7 +4,8 @@ select '"features": [';
 select '{"type":"Feature",'||
         '"properties":{'||
                        '"josm":"'||lower(rm.member_type)||rm.member_id||','||string_agg('r'||rt.relation_id,',' order by rt.relation_id)||'",'||
-                       '"relationtags":"'||string_agg(rtn.k||'|'||replace(rtn.v,'"','\"'),'&' order by rt.relation_id)||'"'||
+                       '"relationtags":"'||string_agg(rtn.k||'|'||replace(rtn.v,'"','\"'),'&' order by rt.relation_id)||'",'||
+                       '"'||replace(coalesce(nt.k,wt.k,''),':','')||'":"'||coalesce(nt.v,wt.v,'')||'"'||
                      '},'||
         '"geometry":'||
              case when rm.member_type='N' then (select st_asgeojson(geom,5) from nodes where id=rm.member_id)
@@ -15,8 +16,10 @@ select '{"type":"Feature",'||
 from relation_tags rt
 inner join relation_members rm on rm.relation_id=rt.relation_id and rm.member_role <> 'is_in'
 left join relation_tags rtn on rtn.relation_id=rt.relation_id and rtn.k='name'
+left join node_tags nt on nt.node_id=rm.member_id and rm.member_type='N' and nt.k='addr:housenumber'
+left join way_tags wt on wt.way_id=rm.member_id and rm.member_type='W' and wt.k='addr:housenumber'
 where rt.k='type' and rt.v like '%treet' 
-group by rm.member_id,rm.member_type
+group by rm.member_id,rm.member_type,nt.k,nt.v,wt.k,wt.v
 having count(*)>1
 order by rm.member_type,rm.member_id;
 select ']}';
