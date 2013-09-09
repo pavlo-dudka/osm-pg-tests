8d6c2d8a2595108e93e6756ca64fead3a8c75c28
pavlo-dudka <pavlo.dudka@gmail.com>
2013-09-09 12:05:56 +0300

    test.highways: added street name check

--- test.highways.bat
+++ test.highways.bat
@@ -3,4 +3,5 @@ start "almost.junctions" "c:\Program Files\PostgreSQL\9.2\bin\psql.exe" -A -t -U
 start "highway.crossings" "c:\Program Files\PostgreSQL\9.2\bin\psql.exe" -A -t -U postgres -w -d osm -p 5432 -f sql\osm.highway.crossings.sql -o results\highway.crossings.geojson -q
 start "no.highways" "c:\Program Files\PostgreSQL\9.2\bin\psql.exe" -A -t -U postgres -w -d osm -p 5432 -f sql\osm.no.highways.sql -o results\no.highways.geojson -q
 start "neighbour.names" "c:\Program Files\PostgreSQL\9.2\bin\psql.exe" -A -t -U postgres -w -d osm -p 5432 -f sql\osm.neighbour.names.sql -o results\osm.neighbour.names.txt -q
+start "street.names" "c:\Program Files\PostgreSQL\9.2\bin\psql.exe" -A -t -U postgres -w -d osm -p 5432 -f sql\osm.street.names.sql -o results\osm.street.names.txt -q
 exit
