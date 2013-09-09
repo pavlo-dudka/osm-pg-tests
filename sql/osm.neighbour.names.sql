9a2af9aece0a9fcb0b2c274ec77893fbeaaf610d
pavlo-dudka <pavlo.dudka@gmail.com>
2013-09-09 14:28:30 +0300

    neighbour.names: refactoring

--- sql/osm.neighbour.names.sql
+++ sql/osm.neighbour.names.sql
@@ -6,22 +6,22 @@ select w1.id, wt1.v as v1, wt2.v as v2, wt3.v as v3, wt2.way_id as road_id
 from 
 ways w1 
   inner join way_tags wt1 on wt1.way_id=w1.id and wt1.k in ('name','addr:street')
-  left  join way_tags wth on wth.way_id=w1.id and wth.k='highway',
+  left  join highways h1  on h1.id=w1.id,
 highways w2 
   inner join way_tags wt2 on wt2.way_id=w2.id and wt2.k='name'
   left  join way_tags wt3 on wt3.way_id=w2.id and wt3.k='name:ru'
 where
  (position(lower(wt1.v) in lower(wt2.v))>0
   or wt1.k='addr:street' and position(lower(wt2.v) in lower(wt1.v))>0 and position('/' in wt1.v)=0 and position(';' in wt1.v)=0
-  or wt1.k='addr:street' and levenshtein_less_equal(lower(wt1.v),lower(wt2.v),4) in (0,1,2,3)
+  or wt1.k='addr:street' and levenshtein_less_equal(lower(wt1.v),lower(wt2.v),4)<4
 
   or position(lower(wt1.v) in lower(wt3.v))>0
   or wt1.k='addr:street' and position(lower(wt3.v) in lower(wt1.v))>0 and position('/' in wt1.v)=0 and position(';' in wt1.v)=0
-  or wt1.k='addr:street' and levenshtein_less_equal(lower(wt1.v),lower(wt3.v),4) in (0,1,2,3)
+  or wt1.k='addr:street' and levenshtein_less_equal(lower(wt1.v),lower(wt3.v),4)<4
   )
 and st_dwithin(w1.linestring,w2.linestring,0.01)
 and wt2.v not like '% улица'
-and (wt1.k='name' and wth.k is not null or wt1.k='addr:street' and wth.k is null))
+and (wt1.k='name' and h1.id is not null or wt1.k='addr:street' and h1.id is null))
 select t.id,v1 as oldv,min(v2) as newv
 from t
 group by 1,2
