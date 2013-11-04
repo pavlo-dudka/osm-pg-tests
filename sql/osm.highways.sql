f6ea47d204d76de3a73ea38361d340b76132c7a7
pavlo-dudka <pavlo.dudka@gmail.com>
2013-11-04 21:19:30 +0200

    exluding platforms

--- sql/osm.highways.sql
+++ sql/osm.highways.sql
@@ -3,7 +3,7 @@ drop table if exists highways;
 create table highways as 
 select w.id, w.linestring, coalesce(wtl.v,'0') as layer
 from ways w
-  inner join way_tags wt on wt.way_id=w.id and wt.k='highway' and wt.v not in ('bus_stop','emergency_access_point')
+  inner join way_tags wt on wt.way_id=w.id and wt.k='highway' and wt.v not in ('bus_stop','emergency_access_point','platform')
   left join  way_tags wtl on wtl.way_id=w.id and wtl.k='layer'
 where not exists(select * from way_tags wta where wta.way_id=w.id and wta.k='area' and wta.v='yes');
 create index idx_highways_id on highways(id);
