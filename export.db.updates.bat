13206182734420468a75c048103756a46bbe1832
pavlo-dudka <pavlo.dudka@gmail.com>
2013-12-25 16:15:11 +0200

    .gitignore

--- export.db.updates.bat
+++
@@ -1,3 +0,0 @@
-call binaries\osmconvert.exe temp\ua.filtered.o5m -o=temp\ua.filtered.pbf
-call binaries\osmosis-latest\bin\osmosis --rs user="postgres" password="postgres" host="localhost:5432" --dd --sort --rb temp\ua.filtered.pbf --sort --dc --wxc ua.diff.osc
-delete temp\ua.filtered.pbf
