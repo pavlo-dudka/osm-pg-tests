428ecd78904fb3cd8fc0ee498df8ad4fc4b5f7b9
pavlo-dudka <pavlo.dudka@gmail.com>
2014-01-08 16:41:50 +0200

    check service roads

--- sql/osm.almost.junctions.sql
+++ sql/osm.almost.junctions.sql
@@ -15,8 +15,8 @@ b as (
   where (select count(*) from way_nodes wn2 where wn.node_id=wn2.node_id group by node_id)=1
   and not exists(select * from node_tags nt where nt.node_id=n.id and nt.k='noexit' and nt.v in ('yes','true','1'))
   and not exists(select * from node_tags nt where nt.node_id=n.id and nt.k='highway' and nt.v='turning_circle')
-  and not exists(select * from way_tags wt where wt.way_id=wn.way_id and wt.k='highway' and wt.v in ('service','platform'))
-  and n.id not in (546405082,546351032,2324552116,1791825888,1616608260,1059524279,1452513637,1452513638,1822002730,1822002733,2394431921,2425800793,1467562604)
+  and not exists(select * from way_tags wt where wt.way_id=wn.way_id and wt.k='highway' and wt.v in ('platform'))
+  and n.id not in (546405082,546351032,2324552116,1791825888,1616608260,1059524279,1452513637,1452513638,1822002730,1822002733,2394431921,2425800793,1467562604,2592172507)
 )
 select * from b;
 
@@ -27,7 +27,9 @@ select '"type": "FeatureCollection",';
 select '"features": [';
 select '{"type":"Feature","id":"'||b.id||'","properties":{"josm":"n'||b.id||','||string_agg('w'||t.id,',' order by t.id)||'"},"geometry":'||st_asgeojson(b.geom,5)||'},'
 from end_nodes b
-inner join highways t on t.id<>b.way_id and t.layer=b.layer and st_dwithin(b.geom,t.linestring,0.01) and st_distance_sphere(b.geom,t.linestring) < 5
+inner join highways t on t.id<>b.way_id and t.layer=b.layer and st_dwithin(b.geom,t.linestring,0.01) and st_distance_sphere(b.geom,t.linestring) < (case when t.highway_level='service' then 1 else 5 end)
+ left join highways h on h.id=b.way_id 
+where st_crosses(h.linestring,t.linestring)='f'
 group by b.id,b.geom
 order by b.id;
 
