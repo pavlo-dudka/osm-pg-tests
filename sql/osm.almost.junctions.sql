f01044ecdcf6080d7ebfc0754ba3bd9ac5375d89
pavlo-dudka <pavlo.dudka@gmail.com>
2014-01-13 14:02:04 +0200

    highway_level

--- sql/osm.almost.junctions.sql
+++ sql/osm.almost.junctions.sql
@@ -1,24 +1,15 @@
 set client_min_messages to warning;
 drop table if exists end_nodes;
 create table end_nodes as
-with 
-a as (
-  select max(sequence_id) as max_sequence_id,wn.way_id,min(t.layer) as layer
-  from way_nodes wn
-  inner join highways t on t.id=wn.way_id
-  group by wn.way_id),
-b as (
-  select a.layer,wn.way_id,n.geom,n.id
-  from a
-  inner join way_nodes wn on wn.way_id=a.way_id and wn.sequence_id in (0,a.max_sequence_id)
-  inner join nodes n on n.id=wn.node_id
-  where (select count(*) from way_nodes wn2 where wn.node_id=wn2.node_id group by node_id)=1
+select h.layer,h.id as way_id,n.geom,n.id
+from highways h
+inner join nodes n on n.id in (h.node0, h.node1)
+where (select count(*) from way_nodes wn2,highways h2 where wn2.node_id=n.id and wn2.way_id=h2.id)=1
   and not exists(select * from node_tags nt where nt.node_id=n.id and nt.k='noexit' and nt.v in ('yes','true','1'))
   and not exists(select * from node_tags nt where nt.node_id=n.id and nt.k='highway' and nt.v='turning_circle')
-  and not exists(select * from way_tags wt where wt.way_id=wn.way_id and wt.k='highway' and wt.v in ('platform'))
+  and not exists(select * from way_tags wt where wt.way_id=h.id and wt.k='highway' and wt.v in ('platform','track'))
   and n.id not in (546405082,546351032,2324552116,1791825888,1616608260,1059524279,1452513637,1452513638,1822002730,1822002733,2394431921,2425800793,1467562604,2592172507)
-)
-select * from b;
+  and h.node0<>h.node1;
 
 select '{';
 select '"type": "FeatureCollection",';
@@ -27,9 +18,9 @@ select '"type": "FeatureCollection",';
 select '"features": [';
 select '{"type":"Feature","id":"'||b.id||'","properties":{"josm":"n'||b.id||','||string_agg('w'||t.id,',' order by t.id)||'"},"geometry":'||st_asgeojson(b.geom,5)||'},'
 from end_nodes b
-inner join highways t on t.id<>b.way_id and t.layer=b.layer and st_dwithin(b.geom,t.linestring,0.01) and st_distance_sphere(b.geom,t.linestring) < (case when t.highway_level='service' then 1 else 5 end)
+inner join highways t on st_dwithin(b.geom,t.linestring,0.01) and t.id<>b.way_id and t.layer=b.layer and st_distance_sphere(b.geom,t.linestring) < (case when t.highway_level='service' then 2 else 5 end)
  left join highways h on h.id=b.way_id 
-where st_crosses(h.linestring,t.linestring)='f'
+where _st_intersects(h.linestring,t.linestring)='f'
 group by b.id,b.geom
 order by b.id;
 
--- sql/osm.highways.sql
+++ sql/osm.highways.sql
@@ -5,9 +5,10 @@ select w.id,
        w.linestring, 
        coalesce(wtl.v,'0') as layer,
        (select node_id from way_nodes wn where wn.way_id=w.id order by wn.sequence_id  asc limit 1) as node0,
-       (select node_id from way_nodes wn where wn.way_id=w.id order by wn.sequence_id desc limit 1) as node1
+       (select node_id from way_nodes wn where wn.way_id=w.id order by wn.sequence_id desc limit 1) as node1,
+       wt.v as highway_level
 from ways w
-  inner join way_tags wt on wt.way_id=w.id and wt.k='highway' and wt.v not in ('bus_stop','emergency_access_point','platform')
+  inner join way_tags wt on wt.way_id=w.id and wt.k='highway' and wt.v not in ('bus_stop','emergency_access_point','platform','footway')
   left join  way_tags wtl on wtl.way_id=w.id and wtl.k='layer'
 where not exists(select * from way_tags wta where wta.way_id=w.id and wta.k='area' and wta.v='yes');
 create index idx_highways_id on highways(id);
