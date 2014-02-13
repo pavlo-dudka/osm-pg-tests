74584db9af7fe55ea5cc1b79dd5da4f69da54a56
pavlo-dudka <pavlo.dudka@gmail.com>
2014-02-13 13:41:58 +0200

    relations crossing border

---
+++ sql/osm.street.relations.o.sql
@@ -0,0 +1,41 @@
+select '{';
+select '"type": "FeatureCollection",';
+select '"features": [';
+
+with t as (
+  select rt.relation_id,(st_dumppoints(w.linestring)).geom geom
+  from relation_tags rt
+    inner join relation_members rm on rm.relation_id=rt.relation_id and rm.member_type='W' and rm.member_role='street'
+    inner join ways w on w.id=rm.member_id),
+t2 as (
+  select relation_id,st_collect(geom) geom
+  from t
+  group by relation_id),
+t3 as (
+  select t2.*,r.id as place_id
+  from t2,
+    relations r inner join relation_tags rt on rt.relation_id=r.id and rt.k='place' and rt.v in ('city','town','village') and r.linestring is not null 
+  where _st_crosses(r.linestring, t2.geom)),
+t4 as (
+  select t3.relation_id,st_difference(t3.geom,r.linestring) diff,coalesce(rt.v,'') as name,coalesce(rp.v,'') as city
+  from t3
+    inner join relations r on r.id=place_id
+    left join relation_tags rt on rt.relation_id=t3.relation_id and rt.k='name'
+    left join relation_tags rp on rp.relation_id=t3.place_id and rp.k='name'),
+t5 as (
+  select *,(select (st_dumppoints(diff)).geom limit 1) geom
+  from t4
+  where t4.relation_id not in (select relation_id from t4 where st_geometrytype(diff)='ST_Point'))
+select '{"type":"Feature",'||
+        '"properties":{'||
+                       '"josm":"r'||t5.relation_id||'",'||
+                       '"relationtags":"name|'||t5.name||'",'||
+                       '"city":"'||t5.city||'",'||
+                     '},'||
+        '"geometry":'||st_asgeojson(t5.geom,5)||
+       '},'
+from t5
+order by 1;
+
+select '{"type":"Feature"}';
+select ']}';
