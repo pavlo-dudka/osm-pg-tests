2d5794419fc4ef56eae8638d65f3fd93d61afe33
pavlo-dudka <pavlo.dudka@gmail.com>
2014-02-15 09:50:33 +0200

    pg_data_folder

--- config.bat
+++ config.bat
@@ -4,3 +4,4 @@
 @set password=postgres
 @set database=osm
 @set psql_exe="c:\Program Files\PostgreSQL\9.3\bin\psql.exe" -h %host% -p %port% -U %username% -w -d %database% -A -t -q
+@set pg_data_folder=C:\Program Files\PostgreSQL\9.3\data\osm\
