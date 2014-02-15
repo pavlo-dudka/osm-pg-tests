f837a68fd28dce87eb109ab09fe1a4c7aed2a34c
pavlo-dudka <pavlo.dudka@gmail.com>
2014-02-15 09:53:08 +0200

    publish.results.now.bat

---
+++ publish.results.now.bat
@@ -0,0 +1,8 @@
+@set publish_path=C:\Users\pdudka.ILS-UA\Dropbox\Public\test
+@set publish_url=http://dl.dropboxusercontent.com/u/14107903/test
+call publish.results.bat
+
+@set publish_path=C:\Users\pdudka.ILS-UA\Downloads\osm-pg-tests-page
+@set publish_url=http://pavlo-dudka.github.io/osm-pg-tests
+call publish.results.bat
+call git-push.bat
