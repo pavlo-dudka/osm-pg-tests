f986a62cb39fe6067fd5b89fc600cd5b084143ef
pavlo-dudka <pavlo.dudka@gmail.com>
2014-02-28 14:35:58 +0200

    exceptions

--- exceptions/osm.load.exceptions.sql
+++ exceptions/osm.load.exceptions.sql
@@ -1,5 +1,5 @@
 drop table if exists exc_street_relations_n;
-create table exc_street_relations_n(street_relation_id int, way_id int, comments text);
+create table exc_street_relations_n(street_relation_id_1 int, street_relation_id_2 int, comments text);
 copy exc_street_relations_n from 'osm/street.relations.n.exc' using delimiters ',';
 
 drop table if exists exc_street_relations_o;
