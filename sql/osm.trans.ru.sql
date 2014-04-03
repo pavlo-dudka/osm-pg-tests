f1bb41f251351080dd22983732f867057ce57f71
pavlo-dudka <pavlo.dudka@gmail.com>
2014-04-03 17:58:52 +0300

    way_type

--- sql/osm.trans.ru.sql
+++ sql/osm.trans.ru.sql
@@ -1,5 +1,6 @@
 select v,string_agg(way_id::text,',' order by way_id) 
-from way_tags 
-where k='name' and v like '% улица' and replace(v,' улица','') not in (select ru from streets) and replace(replace(v,' улица',''),' вулиця','') not in (select uk||' - '||ru from streets union select ru||' - '||uk from streets)
+from way_tags, way_type
+where k='name' and v like '% '||type_f and replace(v,' '||type_f,'') not in (select ru from streets) and replace(replace(v,' '||type_f,''),' '||trans,'') not in (select uk||' - '||ru from streets union select ru||' - '||uk from streets)
+  and lang='ru'
 group by v
 order by 1;
--- sql/osm.trans.uk.sql
+++ sql/osm.trans.uk.sql
@@ -1,5 +1,6 @@
 select v,string_agg(way_id::text,',' order by way_id) 
-from way_tags 
-where k='name' and v like '% вулиця' and replace(v,' вулиця','') not in (select uk from streets) and replace(replace(v,' улица',''),' вулиця','') not in (select uk||' - '||ru from streets union select ru||' - '||uk from streets)
+from way_tags, way_type
+where k='name' and v like '% '||type_f and replace(v,' '||type_f,'') not in (select uk from streets) and replace(replace(v,' '||type_f,''),' '||trans,'') not in (select uk||' - '||ru from streets union select ru||' - '||uk from streets)
+  and lang='uk'
 group by v
 order by 1;
