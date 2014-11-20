b0a492d3e2f8203e066191101c39199a80cadf11
pavlo-dudka <pavlo.dudka@gmail.com>
2014-11-20 15:43:19 +0200

    moved data loading

--- sql/osm.trans.errors.sql
+++ sql/osm.trans.errors.sql
@@ -1,54 +1,4 @@
 drop table if exists streets;
-drop table if exists way_type;
-
-create table way_type as
-select 'uk' as lang,'вулиця' as type_f,'улица' as trans, 'вул(.|иця)' as reg union
-select 'ru' as lang,'улица' as type_f,'вулиця' as trans, '' as reg union
-select 'uk' as lang,'провулок' as type_f,'переулок' as trans, 'пров(.|улок)' as reg union
-select 'ru' as lang,'переулок' as type_f,'провулок' as trans, '' as reg union
-select 'uk' as lang,'площа' as type_f,'площадь' as trans, '' as reg union
-select 'ru' as lang,'площадь' as type_f,'площа' as trans, '' as reg union
-select 'uk' as lang,'майдан' as type_f,null as trans, '' as reg union
-select 'ru' as lang,'майдан' as type_f,'майдан' as trans, '' as reg union
-select 'uk' as lang,'узвіз' as type_f,'спуск' as trans, '' as reg union
-select 'uk' as lang,'спуск' as type_f,'спуск' as trans, '' as reg union
-select 'ru' as lang,'спуск' as type_f,null as trans, '' as reg union
-select 'uk' as lang,'міст' as type_f,'мост' as trans, '' as reg union
-select 'ru' as lang,'мост' as type_f,'міст' as trans, '' as reg union
-select 'uk' as lang,'проїзд' as type_f,'проезд' as trans, '' as reg union
-select 'ru' as lang,'проезд' as type_f,'проїзд' as trans, '' as reg union
-select 'uk' as lang,'набережна' as type_f,'набережная' as trans, '' as reg union
-select 'ru' as lang,'набережная' as type_f,'набережна' as trans, '' as reg union
-select 'uk' as lang,'шосе' as type_f,'шоссе' as trans, '' as reg union
-select 'ru' as lang,'шоссе' as type_f,'шосе' as trans, '' as reg union
-select 'uk' as lang,'алея' as type_f,'аллея' as trans, '' as reg union
-select 'ru' as lang,'аллея' as type_f,'алея' as trans, '' as reg union
-select 'uk' as lang,'в’їзд' as type_f,'въезд' as trans, '' as reg union
-select 'ru' as lang,'въезд' as type_f,'в’їзд' as trans, '' as reg union
-select 'uk' as lang,'підйом' as type_f,'подъём' as trans, '' as reg union
-select 'ru' as lang,'подъём' as type_f,'підйом' as trans, '' as reg union
-select 'uk' as lang,'лінія' as type_f,'линия' as trans, '' as reg union
-select 'ru' as lang,'линия' as type_f,'лінія' as trans, '' as reg union
-select 'uk' as lang,'шляхопровід' as type_f,'путепровод' as trans, '' as reg union
-select 'ru' as lang,'путепровод' as type_f,'шляхопровід' as trans, '' as reg union
-select 'uk' as lang,'автомагістраль' as type_f,'автомагистраль' as trans, '' as reg union
-select 'ru' as lang,'автомагистраль' as type_f,'автомагістраль' as trans, '' as reg union
-select 'uk' as lang,'завулок' as type_f,'заулок' as trans, '' as reg union
-select 'ru' as lang,'заулок' as type_f,'завулок' as trans, '' as reg union
-select 'uk' as lang,'заїзд' as type_f,'заезд' as trans, '' as reg union
-select 'ru' as lang,'заезд' as type_f,'заїзд' as trans, '' as reg union
-select 'uk' as lang,'траса' as type_f,'трасса' as trans, '' as reg union
-select 'ru' as lang,'трасса' as type_f,'траса' as trans, '' as reg union
-select 'uk' as lang,'кільце' as type_f,'кольцо' as trans, '' as reg union
-select 'ru' as lang,'кольцо' as type_f,'кільце' as trans, '' as reg union
-select null as lang,'проспект' as type_f,'проспект' as trans, '' as reg union
-select null as lang,'бульвар' as type_f,'бульвар' as trans, '' as reg union
-select null as lang,'тупик' as type_f,'тупик' as trans, '' as reg union
-select null as lang,'дорога' as type_f,'дорога' as trans, '' as reg union
-select null as lang,'квартал' as type_f,'квартал' as trans, '' as reg union
-select null as lang,'сквер' as type_f,'сквер' as trans, '' as reg union
-select null as lang,'тоннель' as type_f,'тоннель' as trans, '' as reg;
-update way_type set reg=type_f where reg='';
 
 create table streets as
 select substr(name_uk,1,length(name_uk)-length(wtuk.type_f)-1) as uk,
@@ -60,14 +10,14 @@ select
 --wt.v,
 (case when wt2.v is not null then wt2.v 
       when wt.v similar to '%(і|ї|є|’)%' then wt.v 
-      when wt.v similar to '% вулиця' then wt.v 
-      when exists(select 1 from way_type where lang='uk' and wt.v like '% '||type_f) then wt.v
+      when wt.v like '% вулиця' then wt.v 
+      when exists(select 1 from way_type where lang='uk' and wt.v like '% '||type_f and type_f<>trans) then wt.v
       else '-'
  end) as name_uk,
 (case when wt3.v is not null then wt3.v 
       when wt.v similar to '%(ы|ё|ъ)%' then wt.v 
-      when wt.v similar to '% улица' then wt.v 	
-      when exists(select 1 from way_type where lang='ru' and wt.v like '% '||type_f) then wt.v      
+      when wt.v like '% улица' then wt.v 	
+      when exists(select 1 from way_type where lang='ru' and wt.v like '% '||type_f and type_f<>trans) then wt.v      
       else '-'
  end) as name_ru,
 --wt4.v as name_en,
@@ -145,8 +95,8 @@ from highways
 inner join way_tags wt on wt.way_id=id and wt.k='name'
 left join way_tags wtu on wtu.way_id=id and wtu.k='name:uk'
 inner join way_tags wtr on wtr.way_id=id and wtr.k='name:ru'
-where exists(select * from way_type where coalesce(wtu.v,wt.v) similar to type_f||' %|% '||type_f and coalesce(lang,'uk')='uk')
-  and not exists(select * from way_type where coalesce(wtu.v,wt.v) similar to type_f||' %|% '||type_f and coalesce(lang,'uk')='uk' and lower(wtr.v) similar to trans||' %|% '||trans)
+where exists(select * from way_type where coalesce(wtu.v,wt.v) similar to type_f||' %|% '||type_f and coalesce(lang,'uk')='uk' and trans is not null)
+  and not exists(select * from way_type where coalesce(wtu.v,wt.v) similar to type_f||' %|% '||type_f and coalesce(lang,'uk')='uk' and lower(wtr.v) similar to trans||' %|% '||trans and trans is not null)
   and wt.v<>wtr.v
 union
 select id,wt.v,wtu.v,wtr.v
@@ -154,8 +104,8 @@ from highways
 inner join way_tags wt on wt.way_id=id and wt.k='name'
 left join way_tags wtu on wtu.way_id=id and wtu.k='name:uk'
 inner join way_tags wtr on wtr.way_id=id and wtr.k='name:ru'
-where exists(select * from way_type where wtr.v similar to trans||' %|% '||trans and coalesce(lang,'uk')='uk')
-  and not exists(select * from way_type where coalesce(wtu.v,wt.v) similar to type_f||' %|% '||type_f and coalesce(lang,'uk')='uk' and wtr.v similar to trans||' %|% '||trans)
+where exists(select * from way_type where wtr.v similar to trans||' %|% '||trans and coalesce(lang,'uk')='uk' and trans is not null)
+  and not exists(select * from way_type where coalesce(wtu.v,wt.v) similar to type_f||' %|% '||type_f and coalesce(lang,'uk')='uk' and wtr.v similar to trans||' %|% '||trans and trans is not null)
   and wt.v<>wtr.v
   and wtr.v not similar to '% (спуск|площадь)'  
 order by 1;
