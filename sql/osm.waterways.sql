7878ab2ff233005a1f2b53689920e49a574fcdf3
pavlo-dudka <pavlo.dudka@gmail.com>
2014-11-20 15:44:09 +0200

    waterways layer

---
+++ sql/osm.waterways.sql
@@ -0,0 +1,19 @@
+select '{';
+select '"type": "FeatureCollection",';
+select '"errorDescr": "Check waterway layer",';
+select '"features": [';
+
+select '{"type":"Feature","properties":{"josm":"w'||w.id||'","name":"'||coalesce(wtn.v,'')||'","level":"'||wtl.v||'","length":"'||round((st_length(w.linestring::geography)/10))/100||'km"},"geometry":'||st_asgeojson(w.linestring, 5)||'},'
+from ways w
+  inner join way_tags wt on wt.way_id=w.id
+  inner join way_tags wtl on wtl.way_id=w.id
+  left  join way_tags wtn on wtn.way_id=w.id and wtn.k='name'
+where wt.k='waterway' and wt.v not in ('dam','weir') and wtl.k='layer'
+  and w.id not in (select way_id from way_tags where k='tunnel')
+  and w.id not in (select way_id from way_tags where k='bridge' and v='aqueduct')
+  and wtl.v::int not between 0 and 0
+order by st_length(w.linestring::geography) desc
+limit 100;
+
+select '{"type":"Feature"}';
+select ']}';
