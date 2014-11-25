022d7219b9bb5499f5f48ba7b93b85bd776e1ebf
pavlo-dudka <pavlo.dudka@gmail.com>
2014-11-25 15:53:27 +0200

    numeric length

--- sql/osm.waterways.sql
+++ sql/osm.waterways.sql
@@ -3,7 +3,7 @@ select '"type": "FeatureCollection",';
 select '"errorDescr": "Check waterway layer",';
 select '"features": [';
 
-select '{"type":"Feature","properties":{"josm":"w'||w.id||'","name":"'||coalesce(wtn.v,'')||'","level":"'||wtl.v||'","length":"'||round((st_length(w.linestring::geography)/10))/100||'km"},"geometry":'||st_asgeojson(w.linestring, 5)||'},'
+select '{"type":"Feature","properties":{"josm":"w'||w.id||'","name":"'||coalesce(wtn.v,'')||'","level":"'||wtl.v||'","length":"'||round((st_length(w.linestring::geography)/10))/100||'"},"geometry":'||st_asgeojson(w.linestring, 5)||'},'
 from ways w
   inner join way_tags wt on wt.way_id=w.id
   inner join way_tags wtl on wtl.way_id=w.id
