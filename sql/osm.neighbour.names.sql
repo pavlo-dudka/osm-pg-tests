c17c296fcc972ed32b1aeaf40de19a5fa965911e
pavlo-dudka <pavlo.dudka@gmail.com>
2014-12-22 14:51:14 +0200

    check neighbour nodes and relations

--- sql/osm.neighbour.names.sql
+++ sql/osm.neighbour.names.sql
@@ -1,38 +1,89 @@
-﻿set client_min_messages to warning;
-drop table if exists addr_errors;
-create table addr_errors as
+﻿select 'Nodes:';
+with t as (
+select w1.id, wt1.v as v1, wt2.v as v2, wt3.v as v3, wt2.way_id as road_id
+from 
+nodes w1 
+  inner join node_tags wt1 on wt1.node_id=w1.id and wt1.k='addr:street',
+highways w2 
+  inner join way_tags wt2 on wt2.way_id=w2.id and wt2.k='name'
+  inner join way_tags wt3 on wt3.way_id=w2.id and wt3.k like '%name%'
+where
+ (position(lower(wt1.v) in lower(wt3.v))>0
+  or position(lower(wt3.v) in lower(wt1.v))>0 and position('/' in wt1.v)=0 and position(';' in wt1.v)=0
+  or levenshtein_less_equal(lower(wt1.v),lower(wt3.v),4)<4
+  )
+and st_dwithin(w1.geom,w2.linestring,0.01)
+and not exists(select * from relation_tags rt,relation_members rm where rt.relation_id=rm.relation_id and rm.member_id=w1.id and rt.k='type' and rt.v='associatedStreet')
+), addr_errors as (
+select t.id, v1 as oldv, string_agg(distinct v2, '; ' order by v2) as newv, array_agg(distinct road_id) as road_ids
+from t
+group by 1,2
+having not(v1 = any(array_agg(v2))))
+select r.name, oldv, newv, string_agg(distinct a.id::text,',' order by a.id::text), array_to_string(array_agg(distinct road_id), ',') as road_ids
+from addr_errors a
+  inner join nodes w on w.id=a.id
+  left join regions r on st_contains(r.linestring,w.geom),
+  unnest(road_ids) road_id
+group by 1,2,3
+order by 1,2,3;
+
+select '';
+select 'Ways:';
 with t as (
 select w1.id, wt1.v as v1, wt2.v as v2, wt3.v as v3, wt2.way_id as road_id
 from 
 ways w1 
-  inner join way_tags wt1 on wt1.way_id=w1.id and wt1.k in ('name','addr:street')
-  left  join highways h1  on h1.id=w1.id,
+  inner join way_tags wt1 on wt1.way_id=w1.id and wt1.k='addr:street',
 highways w2 
   inner join way_tags wt2 on wt2.way_id=w2.id and wt2.k='name'
-  left  join way_tags wt3 on wt3.way_id=w2.id and wt3.k='name:ru'
+  inner join way_tags wt3 on wt3.way_id=w2.id and wt3.k like '%name%'
 where
- (position(lower(wt1.v) in lower(wt2.v))>0
-  or wt1.k='addr:street' and position(lower(wt2.v) in lower(wt1.v))>0 and position('/' in wt1.v)=0 and position(';' in wt1.v)=0
-  or wt1.k='addr:street' and levenshtein_less_equal(lower(wt1.v),lower(wt2.v),4)<4
-
-  or position(lower(wt1.v) in lower(wt3.v))>0
-  or wt1.k='addr:street' and position(lower(wt3.v) in lower(wt1.v))>0 and position('/' in wt1.v)=0 and position(';' in wt1.v)=0
-  or wt1.k='addr:street' and levenshtein_less_equal(lower(wt1.v),lower(wt3.v),4)<4
+ (position(lower(wt1.v) in lower(wt3.v))>0
+  or position(lower(wt3.v) in lower(wt1.v))>0 and position('/' in wt1.v)=0 and position(';' in wt1.v)=0
+  or levenshtein_less_equal(lower(wt1.v),lower(wt3.v),4)<4
   )
 and st_dwithin(w1.linestring,w2.linestring,0.01)
 and not exists(select * from relation_tags rt,relation_members rm where rt.relation_id=rm.relation_id and rm.member_id=w1.id and rt.k='type' and rt.v='associatedStreet')
---and wt2.v not like '% улица'
-and (wt1.k='name' and h1.id is not null or wt1.k='addr:street' and h1.id is null))
-select t.id,v1 as oldv,string_agg(distinct v2, ';  ' order by v2) as newv
+), addr_errors as (
+select t.id, v1 as oldv, string_agg(distinct v2, '; ' order by v2) as newv, array_agg(distinct road_id) as road_ids
 from t
 group by 1,2
-having not(v1 = any(array_agg(v2)));
-
-select 'Buildings:';
-select r.name,oldv,newv,string_agg(a.id::text,',' order by a.id) 
+having not(v1 = any(array_agg(v2))))
+select r.name, oldv, newv, string_agg(distinct a.id::text,',' order by a.id::text), array_to_string(array_agg(distinct road_id), ',') as road_ids
 from addr_errors a
   inner join ways w on w.id=a.id
-  left join regions r on st_contains(r.linestring,w.linestring)
+  left join regions r on st_contains(r.linestring,w.linestring),
+  unnest(road_ids) road_id
+group by 1,2,3
+order by 1,2,3;
+
+select '';
+select 'Relations:';
+with t as (
+select w1.id, wt1.v as v1, wt2.v as v2, wt3.v as v3, wt2.way_id as road_id
+from 
+relations w1 
+  inner join relation_tags wt1 on wt1.relation_id=w1.id and wt1.k='addr:street',
+highways w2 
+  inner join way_tags wt2 on wt2.way_id=w2.id and wt2.k='name'
+  inner join way_tags wt3 on wt3.way_id=w2.id and wt3.k like '%name%'
+where
+ (position(lower(wt1.v) in lower(wt3.v))>0
+  or position(lower(wt3.v) in lower(wt1.v))>0 and position('/' in wt1.v)=0 and position(';' in wt1.v)=0
+  or levenshtein_less_equal(lower(wt1.v),lower(wt3.v),4)<4
+  )
+and st_dwithin(w1.linestring,w2.linestring,0.01)
+and not exists(select * from relation_tags rt,relation_members rm where rt.relation_id=rm.relation_id and rm.member_id=w1.id and rt.k='type' and rt.v='associatedStreet')
+), addr_errors as (
+select t.id, v1 as oldv, string_agg(distinct v2, '; ' order by v2) as newv, array_agg(distinct road_id) as road_ids
+from t
+group by 1,2
+having not(v1 = any(array_agg(v2))))
+select r.name, oldv, newv, string_agg(distinct a.id::text,',' order by a.id::text), array_to_string(array_agg(distinct road_id), ',') as road_ids
+from addr_errors a
+  inner join relations w on w.id=a.id
+  left join regions r on st_contains(r.linestring,w.linestring),
+  unnest(road_ids) road_id
 group by 1,2,3
 order by 1,2,3;
 
--- sql/osm.street.relations.n.sql
+++ sql/osm.street.relations.n.sql
@@ -12,15 +12,20 @@ with t as (
 t2 as (
   select relation_id,st_collect(geom) geom
   from t
-  group by relation_id)
-select '{"type":"Feature",'||
-        '"properties":{'||
-                       '"josm":"r'||t2.relation_id||','||string_agg(distinct 'w'||w2.id::text,',' order by 'w'||w2.id::text)||'",'||
-                       '"relationtags":"name|'||replace(rtn.v,'"','\"')||'",'||
-                     '},'||
-        '"geometry":'||
-			st_asgeojson((select (st_dumppoints(t2.geom)).geom limit 1), 5)||
-       '},'
+  group by relation_id),
+t3 as (
+/*select distinct t2.relation_id,'n'||w2.id obj_id,replace(rtn.v,'"','\"') rel_name,t2.geom
+from t2
+inner join relation_tags rtn on rtn.relation_id=t2.relation_id and rtn.k in ('name','name:uk','name:ru')
+inner join nodes w2 on _st_dwithin(t2.geom,w2.geom,0.01)
+inner join node_tags wt2 on wt2.node_id=w2.id and wt2.k in ('addr:street') and wt2.v=rtn.v
+where not exists(select * from relation_members rm2 where rm2.relation_id=t2.relation_id and rm2.member_id=w2.id)
+  and not exists(select * from way_tags wt3 where wt3.way_id=w2.id and wt3.k='highway' and wt3.v in ('service','footway','platform','bus_stop'))
+  and not exists(select * from exc_street_relations_n exc,relation_members rm where exc.street_relation_id_1=t2.relation_id and exc.street_relation_id_2=rm.relation_id and rm.member_id=w2.id)
+  and not exists(select * from exc_street_relations_n exc,relation_members rm where exc.street_relation_id_2=t2.relation_id and exc.street_relation_id_1=rm.relation_id and rm.member_id=w2.id)
+  and exists(select * from node_tags wt3 where wt3.node_id=w2.id and wt3.k in ('addr:housenumber','addr:interpolation'))
+union*/
+select distinct t2.relation_id,'w'||w2.id obj_id,replace(rtn.v,'"','\"') rel_name,t2.geom
 from t2
 inner join relation_tags rtn on rtn.relation_id=t2.relation_id and rtn.k in ('name','name:uk','name:ru')
 inner join ways w2 on _st_dwithin(t2.geom,w2.linestring,0.01)
@@ -30,8 +35,27 @@ where not exists(select * from relation_members rm2 where rm2.relation_id=t2.rel
   and not exists(select * from exc_street_relations_n exc,relation_members rm where exc.street_relation_id_1=t2.relation_id and exc.street_relation_id_2=rm.relation_id and rm.member_id=w2.id)
   and not exists(select * from exc_street_relations_n exc,relation_members rm where exc.street_relation_id_2=t2.relation_id and exc.street_relation_id_1=rm.relation_id and rm.member_id=w2.id)
   and (wt2.k<>'addr:street' or exists(select * from way_tags wt3 where wt3.way_id=w2.id and wt3.k in ('addr:housenumber','addr:interpolation')))
-  --and (wt2.k<>'addr:street' or exists(select * from way_tags wt3 where wt3.way_id=w2.id and wt3.k='building'))
-group by t2.relation_id,t2.geom,rtn.v
+union
+select distinct t2.relation_id,'r'||w2.id obj_id,replace(rtn.v,'"','\"') rel_name,t2.geom
+from t2
+inner join relation_tags rtn on rtn.relation_id=t2.relation_id and rtn.k in ('name','name:uk','name:ru')
+inner join relations w2 on _st_dwithin(t2.geom,w2.linestring,0.01)
+inner join relation_tags wt2 on wt2.relation_id=w2.id and wt2.k in ('addr:street') and wt2.v=rtn.v
+where not exists(select * from relation_members rm2 where rm2.relation_id=t2.relation_id and rm2.member_id=w2.id)
+  and not exists(select * from way_tags wt3 where wt3.way_id=w2.id and wt3.k='highway' and wt3.v in ('service','footway','platform','bus_stop'))
+  and not exists(select * from exc_street_relations_n exc,relation_members rm where exc.street_relation_id_1=t2.relation_id and exc.street_relation_id_2=rm.relation_id and rm.member_id=w2.id)
+  and not exists(select * from exc_street_relations_n exc,relation_members rm where exc.street_relation_id_2=t2.relation_id and exc.street_relation_id_1=rm.relation_id and rm.member_id=w2.id)
+  and exists(select * from relation_tags wt3 where wt3.relation_id=w2.id and wt3.k in ('addr:housenumber','addr:interpolation')))
+select '{"type":"Feature",'||
+        '"properties":{'||
+                       '"josm":"r'||t3.relation_id||','||string_agg(distinct obj_id,',' order by obj_id)||'",'||
+                       '"relationtags":"name|'||rel_name||'",'||
+                     '},'||
+        '"geometry":'||
+			st_asgeojson((select (st_dumppoints(t3.geom)).geom limit 1), 5)||
+       '},'
+from t3
+group by t3.relation_id,t3.geom,t3.rel_name
 order by 1;
 
 select '{"type":"Feature"}';
