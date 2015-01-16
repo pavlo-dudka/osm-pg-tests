aff50f052d105c6697db815ad34cc651d817ff53
pavlo-dudka <pavlo.dudka@gmail.com>
2015-01-16 14:16:31 +0200

    exclude highway=track

--- sql/osm.dead.ends.sql
+++ sql/osm.dead.ends.sql
@@ -12,7 +12,7 @@ order by b.id;
 
 select '{"type":"Feature","id":"'||b.id||'","properties":{"josm":"n'||b.id||',w'||h.id||'","region":"'||r.name||'"},"geometry":'||st_asgeojson(b.geom,5)||'},'
 from end_nodes b
-  inner join highways h on b.way_id=h.id and highway_level not in ('service','construction','proposed')
+  inner join highways h on b.way_id=h.id and highway_level not in ('service','construction','track')
   inner join way_tags wt on wt.way_id=h.id and wt.k='oneway' and wt.v<>'no'
   inner join regions r on _st_containsproperly(r.linestring,b.geom)
 order by b.id;
