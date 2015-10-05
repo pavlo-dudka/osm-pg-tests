125f7db1d9537ee1d86bd613d4804a678bddfb01
pavlo-dudka <pavlo.dudka@gmail.com>
2015-10-05 13:47:45 +0300

    refactored pbf update

--- update.pbf.bat
+++ update.pbf.bat
@@ -2,7 +2,8 @@ md temp
 if not exist temp\UA.osm.pbf (binaries\wget.exe http://data.gis-lab.info/osm_dump/dump/latest/UA.osm.pbf -O temp\UA.osm.pbf)
 move temp\ua.osm.pbf temp\ua.0.pbf
 cd binaries
-osmupdate.exe ..\temp\UA.0.pbf ..\temp\ua.osm.pbf --hour --max-merge=2 -v -B=..\poly\ua.poly --complex-ways --keep-tempfiles --tempfiles=..\temp\osmupdate\
+osmupdate.exe ..\temp\UA.0.pbf ..\temp\UA.1.pbf --hour --max-merge=2 -v --keep-tempfiles --tempfiles=..\temp\osmupdate\
+osmconvert.exe ..\temp\UA.1.pbf -o=..\temp\ua.osm.pbf -B=..\poly\ua.poly --complete-ways --complex-ways
 cd ..
-if exist temp\UA.osm.pbf del temp\ua.0.pbf
+if exist temp\UA.osm.pbf del temp\ua.0.pbf temp\ua.1.pbf
 if exist temp\ua.0.pbf move temp\ua.0.pbf temp\ua.osm.pbf
