e82eb172fd58136cf911f22de5a4b0a7f7459203
Pavlo Dudka <pavlo.dudka@gmail.com>
2015-11-23 11:54:12 +0200

    minute update

--- update.min.pbf.sh
+++ update.min.pbf.sh
@@ -1,23 +1,39 @@
+
 #!/bin/sh
-if [ !-e temp/UA.osm.pbf]
- then
-    wget http://data.gis-lab.info/osm_dump/dump/latest/UA.osm.pbf -O temp/UA.osm.pbf
+
+if [ ! -e temp ]
+  then
+    mkdir temp
 fi
 
-mv temp/ua.osm.pbf temp/ua.0.pbf
+if [ ! -e temp/UA.osm.pbf ]
+  then
+    bin/wget http://data.gis-lab.info/osm_dump/dump/latest/UA.osm.pbf -O temp/UA.osm.pbf
+fi
+
+mv temp/UA.osm.pbf temp/ua.0.pbf
 
 cd bin
-osmupdate ../temp/UA.0.pbf ../temp/ua.osm.pbf --minute -v -B=../poly/ua.poly --complex-ways --keep-tempfiles --tempfiles=../temp/osmupdate/
+
+if [ ! -e osmupdate ]
+  then
+    bin/wget -O - http://m.m.i24.cc/osmupdate.c | cc -x c - -o osmupdate
+  else
+    ./osmupdate ../temp/ua.0.pbf ../temp/ua.1.pbf --minute --max-merge=2 -v --keep-tempfiles --tempfiles=../temp/osmupdate/
+    ./osmconvert ../temp/ua.1.pbf -o=../temp/ua.osm.pbf -B=../poly/ua.poly --complete-ways --complex-ways
+fi
 
 cd ..
+
 if [ -e temp/UA.osm.pbf ]
   then
-    rm temp\ua.0.pbf
+    rm temp/ua.0.pbf
+    rm temp/ua.1.pbf
 fi
 
 if [ -e temp/ua.0.pbf ]
   then
-  mv temp/ua.0.pbf temp/ua.osm.pbf
+    mv temp/ua.0.pbf temp/UA.osm.pbf
 fi
 
 exit 0
