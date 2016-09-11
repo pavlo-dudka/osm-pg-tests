b7a4a0968929825341074e929f5765c4ceae1c30
robot-dudka <robot.dudka@gmail.com>
2016-09-11 23:47:55 +0300

    validation (11/09/2016 23:47)

--- javascripts/histogram.js
+++ javascripts/histogram.js
@@ -5,34 +5,33 @@ function showHistogram()
 	//histogramFile = 'almost.junctions.geojson.csv';
 	
 	var data = new google.visualization.DataTable();
-    data.addColumn('datetime', 'X');
-    data.addColumn('number', 'Errors');
+	data.addColumn('datetime', 'X');
+	data.addColumn('number', geojsonFileName);
 	
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.open('GET', 'http://46.8.44.227/' + histogramDate, false);
 	xmlhttp.send(null);
 	
 	var csv = $.map($.csv.toArrays(xmlhttp.responseText), function(line){
-		return [[new Date(Date.parse(line[1])), parseInt(line[2])]];
+		return [[new Date(Date.parse(line[1].replace(' ','T'))), parseInt(line[2])]];
 	});
 	
 	data.addRows(csv);
 
-    var options = {
-        hAxis: {
-          title: 'Time'
-        },
-        vAxis: {
-          title: 'Errors count'
-        },
-        backgroundColor: '#f1f8e9',
-		//height: '500',
+	var options = {
+	        hAxis: {
+	          title: 'Time'
+	        },
+	        vAxis: {
+	          title: 'Errors count'
+	        },
+	        backgroundColor: '#f1f8e9',
 		explorer: {
-			maxZoomOut:2,
-			keepInBounds: true
+		  maxZoomOut:2,
+		  keepInBounds: true
 		}
-    };
+	};
 
-    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-    chart.draw(data, options);
+	var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+	chart.draw(data, options);
 }
