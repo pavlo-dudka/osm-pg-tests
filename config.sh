29524d1e6155ecb8364812e6b62a95d39566d692
root <root@7dd18f2b56a5>
2019-11-04 14:27:10 +0200

    postgres 9.6

--- config.sh
+++ config.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 host=localhost
-port=5434
+port=5432
 username=postgres
 password=postgres
 database=osm
 pgbin_folder=
 psql_exe="psql -p $port -U $username -w -d $database -A -t -q"
-pg_data_folder=/var/lib/postgresql/9.5/main/osm
+pg_data_folder=/var/lib/postgresql/9.6/main/osm
 if [ ! -e $pg_data_folder ]
   then
     mkdir $pg_data_folder
